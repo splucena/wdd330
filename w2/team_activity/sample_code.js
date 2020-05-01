55e939231a1b3888daaec1fe1c754f9fd3f84993
splucena <sumilangplucena@yahoo.com.ph>
2020-05-01 11:09:17 +0800

    added comments on the javascript file

--- w2/team_activity/sample_code.js
+++ w2/team_activity/sample_code.js
@@ -1,13 +1,9 @@
 // Activity #1
 function sum(n) {
-    //console.log(typeof (n));
+    // convert input to float
     const number1 = parseFloat(n);
-    //console.log(number1);
-    //console.log(typeof number1);
-    console.log(parseFloat('1'));
-    console.log(parseFloat('w'));
-    console.log(NaN === parseFloat('1'));
 
+    // check if input is a number
     if (!isNaN(number1)) {
         let total = 0;
         for (let i = 0; i <= n; i++) {
@@ -17,23 +13,28 @@ function sum(n) {
     } else return 'Enter a valid number.';
 }
 
+// display result on the browser
 function displayValue() {
     const number = document.getElementById('text-entry').value;
     total = sum(number);
     document.getElementById('text-entry-container').innerHTML = total;
 }
+
+// instead of calling onclick from html I have attached the
+// click event to the submit button
 document.getElementById('submit').addEventListener('click', displayValue);
 
 // Activity #2
 // function expression
 const computeExpression = (num_1, operation, num_2) => {
-    //console.log(parseFloat(num1));
-    //console.log(parseFloat(num2) === NaN);
+    // initialize and convert each variable
     const num1 = parseFloat(num_1);
     const num2 = parseFloat(num_2);
 
+    // check if both input are numbers
     if (!isNaN(num1) & !isNaN(num2)) {
-        console.log(1);
+        // peform the arithmetic operation based on the button pressed
+        // by the user
         switch (operation) {
             case '+':
                 return Number(num1) + Number(num2);
@@ -44,14 +45,11 @@ const computeExpression = (num_1, operation, num_2) => {
             default:
                 return num1 / num2;
         }
-    }
-    console.log(2);
-    return 'Enter a valid number.';
+    } else return 'Enter a valid number.';
 }
 
-// arrow function
-
-
+// traditional function
+// another way of writing the computeExpression function
 function compute(num1, operation, num2) {
     switch (operation) {
         case '+':
@@ -65,9 +63,9 @@ function compute(num1, operation, num2) {
     }
 }
 
+// display result on the browser
 function displayArithmeticOperationValue(e) {
     let operation = e.target.value;
-    //let operation = '+';
     let num1 = document.getElementById('number-1').value;
     let num2 = document.getElementById('number-2').value;
 
@@ -75,14 +73,18 @@ function displayArithmeticOperationValue(e) {
     document.getElementById('arithmetic-result').innerHTML = result;
 }
 
+// attach click event to the add button
 const addButton = document.getElementById('addition-submit');
 addButton.addEventListener('click', displayArithmeticOperationValue);
 
+// attach click event to the subtract button
 const subtractionButton = document.getElementById('subtraction-submit');
 subtractionButton.addEventListener('click', displayArithmeticOperationValue);
 
+// attach click event to the multiplication button
 const multiplicationButton = document.getElementById('multiplication-submit');
 multiplicationButton.addEventListener('click', displayArithmeticOperationValue);
 
+// attach click event to the division button
 const divisionButton = document.getElementById('division-submit');
 divisionButton.addEventListener('click', displayArithmeticOperationValue);
