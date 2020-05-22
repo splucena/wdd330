8f4913c6e8f9200a1dca37f2f85aef9c6b37195f
splucena <sumilangplucena@yahoo.com.ph>
2020-05-22 14:40:56 +0800

    Updated style

--- w5/todo_challenge/todos.js
+++ w5/todo_challenge/todos.js
@@ -9,3 +9,12 @@ let todo3 = new Todo('test2', false);
 let todos = [todo1, todo2, todo3];
 
 //debugger;
+localStorage.setItem('todos', JSON.stringify(todos));
+// localStorage.todos = JSON.stringify(todos);
+//console.log(JSON.stringify())
+
+let newTodoList = JSON.parse(localStorage.getItem('todos'));
+// JSON.parse(localStorage.todos);
+newTodoList.push(new Todo('test3', false));
+localStorage.todos = JSON.stringify(newTodoList);
+console.log(newTodoList);
