56a0bdcb10eb6458a8148cfb53da1e5f8e29e405
splucena <sumilangplucena@yahoo.com.ph>
2020-07-01 10:15:48 +0800

    added link

--- final_project/static/js/app.js
+++ final_project/static/js/app.js
@@ -3,7 +3,22 @@
 //const p = new Product();
 //p.getProducts();
 import ProductsController from "./ProductsController.js";
+import CartController from "./CartController.js";
 
-const output = '#product-list';
-const p = new ProductsController(output);
-p.init();
+function showProducts() {
+    const output = '#product-list';
+    const p = new ProductsController(output);
+    p.init();
+}
+showProducts();
+
+const viewProducts = document.querySelector('#view-products');
+viewProducts.addEventListener('click', e => {
+    showProducts();
+})
+
+const viewCart = document.querySelector('#view-cart');
+viewCart.addEventListener('click', e => {
+    const cart = new CartController('#product-list');
+    cart.showAllProducts();
+})
